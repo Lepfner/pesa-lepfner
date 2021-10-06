5720149d356c5ea4dd3ecd742627e72cea32c642
Lepfner <lerner.andi@gmail.com>
2021-10-06 23:56:51 +0200

    Added font awesome

--- pesa-tours-frontend/src/pages/DestinationComponent.js
+++ pesa-tours-frontend/src/pages/DestinationComponent.js
@@ -4,6 +4,9 @@ import MetaTags from "react-meta-tags";
 
 import Footer from "../components/Footer";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShip} from '@fortawesome/free-solid-svg-icons';
+
 function Destination({mapItem}) {
   return (
     <>
@@ -12,6 +15,7 @@ function Destination({mapItem}) {
                   src="https://images.unsplash.com/photo-1463320898484-cdee8141c787?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
                   alt=""
               />
+              <FontAwesomeIcon icon={faShip} color="white" className="h-6 absolute top-8 left-8"/>
               <div class="p-4 bg-white">
                   <h1 class="mt-4 font-bold text-2xl text-ocean">{mapItem.name}</h1>
                   <p className="text-ocean">
